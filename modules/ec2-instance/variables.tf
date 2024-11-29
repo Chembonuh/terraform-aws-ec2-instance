cdcfcd22121b69d4bbfd7f25deb4a81c1a7a76af
Chembonuh <nchembonuh67@gmail.com>
2024-11-29 09:40:08 -0500

    debug

--- modules/ec2-instance/variables.tf
+++ modules/ec2-instance/variables.tf
@@ -3,23 +3,32 @@ variable "region" {
   type        = string
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+variable "instance_ami" {
+  description = "The AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance"
+  description = "Type of the EC2 instance"
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the SSH key to use"
+  description = "The name of the SSH key to use for the instance"
   type        = string
+}
+
+variable "volume_size" {
+  description = "The size of the EBS volume in GB"
+  type        = number
 }
 
 variable "tags" {
   description = "Tags to apply to the instance"
   type        = map(string)
+  default     = {
+    Name        = "DefaultInstance"
+    Environment = "Development"
+  }
 }
