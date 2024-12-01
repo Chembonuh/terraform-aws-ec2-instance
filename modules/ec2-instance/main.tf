58af6d9c4476ce86aaac491f6a27dd6daef5137c
Chembonuh <nchembonuh67@gmail.com>
2024-12-01 11:02:56 -0500

    debug

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -2,15 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
+module "ec2_instance" {
+  source        = "./modules/ec2-instance"  # Path to your module
+  instance_ami  = var.instance_ami          # AMI ID passed as a variable
+  region        = var.region                # AWS region
+  instance_type = var.instance_type         # Instance type
+  key_name      = var.key_name              # Key pair for the instance
+  volume_size   = var.volume_size           # Volume size
 
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    volume_size = var.volume_size
-  }
-
-  tags = var.tags
+  tags = var.tags                           # Tags for the EC2 instance
 }
