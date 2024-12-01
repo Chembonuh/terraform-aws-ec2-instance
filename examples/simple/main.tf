ccad890af46b2e2c3e2b9ea7807e85b6e6387c6c
Chembonuh <nchembonuh67@gmail.com>
2024-12-01 08:39:09 -0500

    correct path

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source        = "../../modules/ec2-instance"
+  source        = "modules/ec2-instance"
   instance_ami  = "ami-0c11a84584d4e09dd"
   region        = "us-east-2"
   instance_type = "t2.micro"
