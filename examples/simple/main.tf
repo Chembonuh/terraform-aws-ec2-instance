14c29e1afdc66f7b89091e3d2557733c15340661
Chembonuh <nchembonuh67@gmail.com>
2024-12-01 08:55:20 -0500

    debug

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -9,12 +9,12 @@ terraform {
   }
 }
 
-provider "aws" {
+provider "aws" ./{
   region = var.region                         # Region is passed as a variable
 }
 
 module "ec2_instance" {
-  source        = "modules/ec2-instance"
+  source        = "./modules/ec2-instance"
   instance_ami  = "ami-0c11a84584d4e09dd"
   region        = "us-east-2"
   instance_type = "t2.micro"
