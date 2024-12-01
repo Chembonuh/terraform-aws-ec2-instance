83a5c880268d794723dcb044156182f9eca78dfd
Your Name <Chembonuh>
2024-12-01 13:47:15 -0600

    chore: nnn

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,29 +1,19 @@
 terraform {
   backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "chem"
+    hostname     = "app.terraform.io" # Terraform Cloud's endpoint
+    organization = "chem"             # Your Terraform Cloud organization
 
     workspaces {
-      name = "terraform-aws-ec2-instance"
+      name = "terraform-aws-ec2-instance" # Name of your Terraform Cloud workspace
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.region # Region is passed as a variable
 }
 
 module "ec2_instance" {
-<<<<<<< HEAD
-  source        = "../../modules/ec2-instance"
-  instance_ami  = var.instance_ami
-  region        = var.region
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  volume_size   = var.volume_size
-
-  tags = var.tags
-=======
   source        = "./modules/ec2-instance"
   region        = var.region
   instance_ami  = var.instance_ami
@@ -31,5 +21,5 @@ module "ec2_instance" {
   volume_size   = var.volume_size
   key_name      = var.key_name
   tags          = var.tags
->>>>>>> origin/main
 }
+
