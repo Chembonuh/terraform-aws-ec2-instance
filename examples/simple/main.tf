e2e5f67a0cd5974880adffda6286a8d868773081
Chembonuh <nchembonuh67@gmail.com>
2024-12-01 12:17:26 -0500

    debug

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source        = "${path.root}/../../modules/ec2-instance"  # Use path.root for proper resolution
+  source        = "${path.root}/modules/ec2-instance"  # Use path.root for proper resolution
   instance_ami  = "ami-0c11a84584d4e09dd"
   region        = "us-east-2"
   instance_type = "t2.micro"
