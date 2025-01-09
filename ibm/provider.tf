93d02e92e941dfd8f7c46be139890490ea738688
Chembonuh <nchembonuh67@gmail.com>
2025-01-09 09:35:36 -0500

    debug

--- ibm/provider.tf
+++ ibm/provider.tf
@@ -1,4 +1,4 @@
-provider "ibm-cloud/ibm" {
+provider "ibm" {
     region  = var.region
     zone    = var.zone
     ibmcloud_api_key = var.ibmcloud_api_key #  ibmcloud_api_key = var.ibmcloud_api_key != null ? var.ibmcloud_api_key : null
