dfbc7efca43e4d048566dac291a07d405d535f00
Chembonuh <nchembonuh67@gmail.com>
2025-01-09 09:34:24 -0500

    update the dam provider

--- ibm/provider.tf
+++ ibm/provider.tf
@@ -1,4 +1,4 @@
-provider "ibm" {
+provider "ibm-cloud/ibm" {
     region  = var.region
     zone    = var.zone
     ibmcloud_api_key = var.ibmcloud_api_key #  ibmcloud_api_key = var.ibmcloud_api_key != null ? var.ibmcloud_api_key : null
