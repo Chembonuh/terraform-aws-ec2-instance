a95e83ab32afc6faff901acb9ad1ad5f99a9f87d
Chembonuh <nchembonuh67@gmail.com>
2025-01-09 10:50:39 -0500

    clean

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  value       = aws_instance.web_server.id
-  description = "ID of the EC2 instance"
-}
-
-output "public_ip" {
-  value       = aws_instance.web_server.public_ip
-  description = "Public IP of the EC2 instance"
-}
